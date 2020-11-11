09c645b1934f6c7e7c91dcd7d546b8fc41893cf4
JohnStrandt <72990852+JohnStrandt@users.noreply.github.com>
2020-11-11 14:33:12 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,38 @@
+// const hamburger = document.querySelector('.hamburger');
+// const navLinks = document.querySelector('.nav-links');
+// const links = document.querySelectorAll('.nav-links li');
 
+// hamburger.addEventListener('click', () => {
+//   navLinks.classList.toggle('open');
+//   links.forEach(link => {
+//     link.classList.toggle('fade');
+//   });
+// });
+
+
+const navLinks = document.querySelector('.nav-links');
+const links = document.querySelectorAll('.nav-links li');
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+
+    navLinks.classList.toggle('open');
+    links.forEach(link => {
+        link.classList.toggle('fade');
+    });
+  } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+
+    navLinks.classList.toggle('open');
+    links.forEach(link => {
+      link.classList.toggle('fade');
+    });
+  }
+
+});
 
 
